44a04aead0dc2c742515ad6793fe1c4b5cc09bec
takehiro <takehiro.watanabe@globis.co.jp>
2018-09-13 12:51:10 +0900

    Initial Commit

---
+++ mvvm-pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-pattern
+//
+//  Created by Watanabe Takehiro on 2018/09/13.
+//  Copyright © 2018年 Watanabe Takehiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
