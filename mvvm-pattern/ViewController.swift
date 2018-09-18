a7f57c02971528c13b89d91d2bb50b21c9ab60c4
takehiro224 <mogu.224@gmail.com>
2018-09-18 23:50:42 +0900

    navigation test

--- mvvm-pattern/ViewController.swift
+++ mvvm-pattern/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let searchBar = UISearchBar()
+        self.navigationItem.titleView = searchBar
+        
     }
 
     @IBAction func btnTapped(_ sender: UIButton) {
-        let sb = self.storyboard!
-        let vc = sb.instantiateViewController(withIdentifier: "second") as! SecondViewController
+//        let sb = self.storyboard!
+//        let vc = sb.instantiateViewController(withIdentifier: "second") as! SecondViewController
 //        self.navigationController?.pushViewController(vc, animated: true)
-        self.present(vc, animated: true, completion: nil)
+//        self.present(vc, animated: true, completion: nil)
 //        self.performSegue(withIdentifier: "test", sender: nil)
     }
 }
