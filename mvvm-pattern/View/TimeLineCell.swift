2cfeb39b4a5caed0721ea222b91371cded209a27
takehiro224 <mogu.224@gmail.com>
2018-09-22 14:18:38 +0900

    名前修正

---
+++ mvvm-pattern/View/TimeLineCell.swift
@@ -0,0 +1,21 @@
+//
+//  TimeLineCell.swift
+//  mvvm-pattern
+//
+//  Created by 渡邊丈洋 on 2018/09/20.
+//  Copyright © 2018年 Watanabe Takehiro. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
